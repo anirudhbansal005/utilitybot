71b7f783c5d39104648083b43c1b4c48f7e269f8
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-02-28 09:37:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord.ext import commands, tasks
+from datetime import datetime, timedelta
 from decouple import config
 
 class MyBot(commands.Bot):
@@ -7,6 +9,8 @@ class MyBot(commands.Bot):
         super().__init__(command_prefix)
         self.load_token()
         self.setup_mongo()
+        self.lockdown_task.start()
+
 
     def load_token(self):
         self.token = config('TOKEN')
@@ -28,6 +32,43 @@ class MyBot(commands.Bot):
     @commands.command(name='test')
     async def test(self, ctx):
         await ctx.send(f'test, {ctx.author.mention}!')
+
+    @commands.command(name='locktime')
+    async def locktime(self, ctx, start_hour: int, duration_hours: int):
+        """Set the start hour and duration for server lockdown.\nExample: //locktime 00h 8h"""
+        collection = self.db['lockdown_times']
+        document = {'start_hour': start_hour, 'duration_hours': duration_hours}
+        await collection.replace_one({}, document, upsert=True)
+        await ctx.send(f"Lockdown time set. Server will be locked down daily starting at {start_hour}:00 for {duration_hours} hours.")
+        
+    @tasks.loop(hours=24)  # Runs every 24 hours
+    async def lockdown_task(self):
+        server_id = t  
+        guild = self.get_guild(server_id)
+        everyone_role = guild.default_role
+
+       
+        collection = self.db['lockdown_times']
+        document = await collection.find_one({})
+        if document:
+            start_hour = document.get('start_hour', 0)
+            duration_hours = document.get('duration_hours', 8)
+            current_hour = datetime.now().hour
+            lockdown_start_time = datetime.now().replace(hour=start_hour, minute=0, second=0, microsecond=0)
+
+            
+            if current_hour == start_hour and datetime.now() >= lockdown_start_time:
+                
+                await everyone_role.edit(send_messages=False)
+                print(f"Server locked down at {datetime.now()}")
+
+                await asyncio.sleep(duration_hours * 3600)
+                
+                await everyone_role.edit(send_messages=True)
+                print(f"Server unlocked at {datetime.now()}")
+
+def run_bot():
+    # Create an instance of your bot class with a command prefix
 
 def run_bot():
     bot = UtilityBot(command_prefix='==')
