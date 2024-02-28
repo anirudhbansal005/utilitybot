7f60f27217ef0b5370dab18a5d784b3d68a22717
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-02-28 09:19:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,14 +6,25 @@ class MyBot(commands.Bot):
     def __init__(self, command_prefix):
         super().__init__(command_prefix)
         self.load_token()
+        self.setup_mongo()
 
     def load_token(self):
-        # Load the token from a .env file
-        self.token = config('DISCORD_BOT_TOKEN')
+        self.token = config('TOKEN')
+
+    def setup_mongo(self):
+        mongo_connection_string = config('CONNECTION_URI')
+        database_name = config('DB_NAME')
+
+        self.mongo_client = motor.motor_asyncio.AsyncIOMotorClient(mongo_connection_string)
+        self.db = self.mongo_client[database_name]
 
     async def on_ready(self):
         print(f'Started {self.user.name}')
-
+        
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+            
     @commands.command(name='test')
     async def test(self, ctx):
         await ctx.send(f'test, {ctx.author.mention}!')
